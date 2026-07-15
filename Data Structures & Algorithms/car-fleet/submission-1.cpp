9a52fc21519be0543fef32bfb56f9d549e76e4ea
Mukul Nayak <78421867+mn-op99@users.noreply.github.com>
2026-07-15 19:37:40 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> v;
+        for(int i=0; i<position.size(); ++i)
+            v.push_back({position[i], speed[i]});
+        
+        sort(v.begin(), v.end());
+
+        int ct=1;
+        auto nextCar=v[v.size()-1];
+        for(int i=v.size()-2; i>=0; --i){
+            double hopsCurr = (double)(target-v[i].first)/v[i].second;
+            double hopsNext = (double)(target-nextCar.first)/nextCar.second;
+            if(hopsCurr<hopsNext)
+                continue;
+            else if(hopsCurr==hopsNext){
+                double finalHC = hopsCurr*v[i].second + v[i].first;
+                double finalHN = hopsNext*nextCar.second + nextCar.first;
+                if(finalHC>=finalHN)
+                    continue;
+                else{
+                    ct++;
+                    nextCar = v[i];
+                }
+            }
+            else{
+                ct++;
+                nextCar = v[i];
+            }
+        }
+        return ct;
+    }
+};
